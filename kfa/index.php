2cb81284f3609e9391835772e7aed74c7762a349
webmasteraafa <53908739+webmasteraafa@users.noreply.github.com>
2020-05-12 10:08:58 -0400

    Create index.php

---
+++ kfa/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE htm PUBLIC "-//W3C//DTD htm 3.2 Final//EN">
+
+<HTML>
+<HEAD>
+<TITLE>Kids With Food Allergies.org</TITLE>
+<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=/index.html">
+</HTML>
